c52d6897dbb948dc3b51cbaf6694f6515a64fcc8
Sebastian <s.bialalearmonth@gmail.com>
2021-04-28 09:43:39 +1000

    Made scripts folder

---
+++ Primitive Resentment/Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Interactable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.enabled = false;
+    }
+
+    public void Interact()
+    {
+        Debug.Log("interacting with" + name);
+    }
+}
