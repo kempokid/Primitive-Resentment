8abe4aa52d12ad26f1b13850836603efd7474a31
Tim <timhinshelwood@gmail.com>
2021-05-12 16:47:32 +1000

    Added Keypad System

---
+++ Primitive Resentment/Assets/Scripts/KeypadManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeypadManager : MonoBehaviour
+{
+
+    public int maxPress;
+    public int currentPress;
+    public TextMeshProUGUI KeypadDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KeypadDisplay.text = null;
+        maxPress = 5;
+        currentPress = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PressKeypad()
+    {
+        if(currentPress < maxPress)
+        {
+            currentPress++;
+        } 
+
+        if(currentPress == 1)
+        {
+            KeypadDisplay.text = "*";
+        }
+
+        if (currentPress == 2)
+        {
+            KeypadDisplay.text = "**";
+        }
+
+        if(currentPress == 3)
+        {
+            KeypadDisplay.text = "***";
+        }
+
+        if(currentPress == 4)
+        {
+            KeypadDisplay.text = "****";
+        }
+        
+        if (currentPress == maxPress)
+        {
+            RTS();
+            KeypadDisplay.text = null;
+            currentPress = 0;
+        }
+    }
+    public string[] randomText;
+    public TextMeshProUGUI insultDisplay;
+
+
+    public void RTS()
+    {
+        insultDisplay.text = GetRandomText();
+    }
+
+    string GetRandomText()
+    {
+        int random = UnityEngine.Random.Range(0, randomText.Length);
+        return randomText[random];
+    }
+}
