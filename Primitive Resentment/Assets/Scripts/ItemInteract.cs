b2410c9c5d16d3da3e9c9be19a1b9b05f631532e
Sebastian <s.bialalearmonth@gmail.com>
2021-05-12 12:29:55 +1000

    Made interactable inventory items

---
+++ Primitive Resentment/Assets/Scripts/ItemInteract.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteract : MonoBehaviour
+{
+    private Dialogue_Manager dialogueManager;
+    public SO_Convo interactDialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueManager = FindObjectOfType<Dialogue_Manager>();
+    }
+
+    public void ObjectInteract()
+    {
+        dialogueManager.Start_Dialogue(interactDialogue);
+    }
+}
