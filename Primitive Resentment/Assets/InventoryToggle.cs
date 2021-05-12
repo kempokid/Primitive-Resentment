d69047aa754cbabe69f1b13622f7922696b45062
Sebastian <s.bialalearmonth@gmail.com>
2021-05-12 15:00:03 +1000

    Made script to toggle inventory

---
+++ Primitive Resentment/Assets/InventoryToggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryToggle : MonoBehaviour
+{
+
+    public GameObject inventoryScreen;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if(inventoryScreen.activeSelf == true)
+            {
+                inventoryScreen.SetActive(false);
+            }
+            else
+            {
+                inventoryScreen.SetActive(true);
+            }
+        }
+    }
+}
