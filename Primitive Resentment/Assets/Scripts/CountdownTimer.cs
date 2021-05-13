1a55da2950dcdad6dfc2855898ae73ed8b9bc620
Tim <timhinshelwood@gmail.com>
2021-05-13 12:37:39 +1000

    Added countdown timer

---
+++ Primitive Resentment/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour
+{
+    private float startingTime = 10f;
+    public float totalTime;
+
+    [Header("Dialogue Options")]
+    public Dialogue_Manager dialogueManager;
+    public SO_Convo myConvo;
+
+    public TextMeshProUGUI clock;
+
+    private float minutes;
+    private float seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+
+        minutes = (int)(totalTime / 60);
+        seconds = (int)(totalTime % 60);
+
+        clock.text = minutes.ToString() + " : " + seconds.ToString();
+
+        if(minutes <= 0f && seconds <= 0)
+        {
+            totalTime = 0f;
+            TimerEvent();
+        }
+    }
+
+    public void TimerEvent()
+    {
+        //Trigger monkey dialogue
+        {
+            dialogueManager.Start_Dialogue(myConvo);
+        }
+
+        //Open door
+
+
+        //Destroy time manager
+        Destroy(gameObject);
+    }
+}
