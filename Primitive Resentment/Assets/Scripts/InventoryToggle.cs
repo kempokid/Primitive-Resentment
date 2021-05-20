1d217f0bf519d66c930105039911b6191b028340
Sebastian <s.bialalearmonth@gmail.com>
2021-05-20 15:09:33 +1000

    Changed the back button and added to the final conflict

--- Primitive Resentment/Assets/Scripts/InventoryToggle.cs
+++ Primitive Resentment/Assets/Scripts/InventoryToggle.cs
@@ -8,7 +8,7 @@ public class InventoryToggle : MonoBehaviour
     public GameObject inventoryScreen;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             if(inventoryScreen.activeSelf == true)
             {
