656103397d60ace92153870366fb7c06792b9d13
Tim <timhinshelwood@gmail.com>
2021-05-20 10:32:23 +1000

    Added Main Menu + Functionality

---
+++ Primitive Resentment/Assets/Scripts/CamSwap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwap : MonoBehaviour
+{
+
+    public GameObject nextCam;
+
+    public void camSwap()
+    {
+        nextCam.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Primitive Resentment/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("ProperScene");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
