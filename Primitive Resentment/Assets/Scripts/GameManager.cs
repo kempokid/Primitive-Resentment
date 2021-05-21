45c7abc18acff0acc5b7cca38d521ca706901ba4
Tim <timhinshelwood@gmail.com>
2021-05-21 11:24:02 +1000

    Added fade out/in screen

---
+++ Primitive Resentment/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private Animation fadeOut;
+    public GameObject fadeScreen;
+    private AnimationClip fadeOutClip;
+
+    private void Start()
+    {
+        fadeOut = fadeScreen.GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            fadeOut.Play("Fade Out");
+        }
+    }
+
+    public void NextLevel()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
