478174bf61ae7e7dfaaa53e62885140a65a622f8
William <wtbeaven@gmail.com>
2021-05-21 14:05:59 +1000

    Added scene transitions

---
+++ Primitive Resentment/Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueButton : MonoBehaviour
+{
+    public GameObject fuckYou;
+
+    // Start is called before the first frame update
+    public void getFucked()
+    {
+        fuckYou.SetActive(true);
+    }
+}
