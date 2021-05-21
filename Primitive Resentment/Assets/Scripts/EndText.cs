06d7c3197dab9af19faa51e469a6de36b8685f22
William <wtbeaven@gmail.com>
2021-05-21 11:58:23 +1000

    Added typewriter text scene

---
+++ Primitive Resentment/Assets/Scripts/EndText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class EndText : MonoBehaviour
+{
+    public float letterPause = 0.2f;
+
+    string message;
+    TextMeshProUGUI textComp;
+
+    // Use this for initialization
+    void Start()
+    {
+        textComp = GetComponent<TextMeshProUGUI>();
+        message = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        
+        foreach (char letter in message)
+        {
+            textComp.text += letter;
+            yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+}
